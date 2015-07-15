0ac42fcbdb7f99eb168660bb1bfd5d9cdf750023
Odei Riveiro Zafra <odei.riveiro@gmail.com>
2015-07-15 16:57:42 +0200

    Agregada consulta de carga de generos

---
+++ public/resources/php/consulta.php
@@ -0,0 +1,28 @@
+<?php
+    try {
+        $json = "*error*";
+        $dbh = new PDO('mysql:host=localhost;dbname=jukebox;charset=utf8','root','root');
+        $dbh -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        if (empty($_GET)) {
+            $consulta = $dbh -> prepare("SELECT DISTINCT genero FROM albumes ORDER BY genero");
+            $consulta -> execute();
+            $generos = $consulta -> fetchAll(PDO::FETCH_ASSOC);
+            $json = json_encode($generos);
+        } else if(isset($_GET['genero'])) {
+            $consulta = $dbh -> prepare("SELECT * FROM albumes WHERE genero=:genero ORDER BY nombre");
+            $consulta -> bindValue(':genero', $_GET['genero'], PDO::PARAM_INT);
+            $consulta -> execute();
+            $albumes = $consulta -> fetchAll(PDO::FETCH_ASSOC);
+            $json = json_encode($albumes);
+        } else if(isset($_GET['album'])) {
+            $consulta = $dbh -> prepare("SELECT * FROM pistas WHERE id_album=:id ORDER BY numero");
+            $consulta -> bindValue(':id', $_GET['album'], PDO::PARAM_INT);
+            $consulta -> execute();
+            $pistas = $consulta -> fetchAll(PDO::FETCH_ASSOC);
+            $json = json_encode($pistas);
+        }
+        echo $json;     //vector devuelto en formato json(array asociativo)
+    } catch (PDOException $e) {
+        die('Ha ocurrido un *error* al acceder a la BBDD.');
+    }
+?>
