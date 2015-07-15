765bb79cd9826d4f5fe5b976214df24482ef2c48
Odei Riveiro Zafra <odei.riveiro@gmail.com>
2015-07-15 18:11:25 +0200

    Actualizadas referencias entre ficheros

--- public/resources/js/script.js
+++ public/resources/js/script.js
@@ -1,31 +1,31 @@
 $(document).ready(function(){
-    $.get('recursos/php/consulta.php', function(generosJson) {
+    $.get('resources/php/consulta.php', function(generosJson) {
         $.each(JSON.parse(generosJson), function(i, item) {
             $('<a/>', {
                 text: item.genero, id: 'btn' + i, href: '#btn' + i, appendTo: '#botones',
                 click: function () {
-                    new Audio("recursos/mp3/radio-tune.mp3").play();
-                    $.get('recursos/php/consulta.php', { genero: item.genero }, function(albumesJson) {
+                    new Audio("resources/mp3/radio-tune.mp3").play();
+                    $.get('resources/php/consulta.php', { genero: item.genero }, function(albumesJson) {
                         $("#seleccion").html('');
                         var slc = $("<select />", {
                             change: function () {
-                                new Audio("recursos/mp3/disco-tune.mp3").play();
+                                new Audio("resources/mp3/disco-tune.mp3").play();
                                 $("#jukebox").css({"border-bottom":"none", "padding-bottom":"10px"});
-                                $.get('recursos/php/consulta.php', { album: this.value }, function(pistasJson) {
+                                $.get('resources/php/consulta.php', { album: this.value }, function(pistasJson) {
                                     var indice = $('#seleccion select')[0].selectedIndex -1;
                                     var albumes = JSON.parse(albumesJson);
-                                    $("#cristal img:first").attr("src", 'recursos/img/' + albumes[indice].portada + '.jpg');
+                                    $("#cristal img:first").attr("src", 'resources/img/' + albumes[indice].portada + '.jpg');
                                     $("#cristal span:first").text(albumes[indice].nombre + ' (' + albumes[indice].anio + ') - ' + albumes[indice].artista).circleType({radius: 190});
                                     var ul = $("<ul />").append($('<li/>', { text: 'Traklist' }));
                                     $.each(JSON.parse(pistasJson), function(i, item) {
                                         $('<li/>', {
                                             text: item.numero + '. ' + item.nombre, appendTo: ul,
                                             click: function () {
-                                                $("#led").attr('src','recursos/img/led-on.gif');
+                                                $("#led").attr('src','resources/img/led-on.gif');
                                                 $("audio").remove();
                                                 $("<audio></audio>").attr({         //si no encuentra el mp3 o no lo reproduce se cambia la imagen
-                                                        'src': "recursos/mp3/" + item.nombre + ".mp3", 'autoplay':'autoplay', 'controls': true
-                                                    }).prependTo("#jukebox").on({ error: function(){ $("#led").attr('src','recursos/img/led-off.gif'); }
+                                                        'src': "resources/mp3/" + item.nombre + ".mp3", 'autoplay':'autoplay', 'controls': true
+                                                    }).prependTo("#jukebox").on({ error: function(){ $("#led").attr('src','resources/img/led-off.gif'); }
                                                 });
                                             }
                                         });
